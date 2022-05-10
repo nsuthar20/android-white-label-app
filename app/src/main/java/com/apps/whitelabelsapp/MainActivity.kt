7c0f28410e570cc37c8f0293d3c837915d065602
User <userxyz@xyzz.xom>
2022-05-10 12:10:14 +0530

    Showing white-label-app's flavor name in TextView using BuildConfig file.

--- app/src/main/java/com/apps/whitelabelsapp/MainActivity.kt
+++ app/src/main/java/com/apps/whitelabelsapp/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.apps.whitelabelsapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView: TextView = findViewById(R.id.text_view)
+        textView.text = BuildConfig.FLAVOR
     }
 }
